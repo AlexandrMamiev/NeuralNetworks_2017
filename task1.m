051b48ab717348821208187eebd3165fad24c0f2
user <you@example.com>
2017-12-06 09:38:00 +0400

    task1

--- task1.m
+++ task1.m
@@ -1,18 +1,18 @@
-a = 2; 
-b = 4.1;
-xn=0.77; 
-xk=1.77; 
-dx=0.2;
-X=[1.24,1.38,2.38,3.21,0.68]; 
+a = 7.2; 
+b = 4.2;
+xn=1.81; 
+xk=5.31; 
+dx=0.7;
+X=[2.4,2.8,3.9,4.7,3.16]; 
 X=sort(X);
 x=xn:dx:xk;
 
 function yres=yCalc(x,a,b)
-  chisl = log(b^2 - x.^2)/log(a);
-  znam = (abs(x.^2 - a^2)).^(1/3);
+  chisl = abs(a - (b * x));
+  znam = log(x).^3;
   yres = chisl./znam;
 end
-
+  
 y1 = yCalc(x,a,b);
 y2 = yCalc(X,a,b);
 
